d9f1b18a811191714036f2d0b7f23a6b695c42d1
Maliniak, Michael <Michael.Maliniak@news.com.au>
2016-11-25 11:11:47 +1100

    Mike query stuff

---
+++ app.js
@@ -0,0 +1,13 @@
+$( document ).ready(function() {
+
+    // hide all card bottom sections 
+    $('.card-bottom-section').hide();
+    // slide toggle bottom sections.
+    $('.card-toggle').on('click', function() {
+        $el = $(this);
+        $el.next().slideToggle();
+        // toggle text
+        var text = $el.text();
+        $el.text(text == "Read More" ? "Read Less" : "Read More");
+    });
+});
