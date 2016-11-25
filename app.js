1093136ad5535bd330216bc1dc7ef732b6621567
Maliniak, Michael <Michael.Maliniak@news.com.au>
2016-11-25 12:36:36 +1100

    query thermometer

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 $( document ).ready(function() {
-
-    // hide all card bottom sections 
-    $('.card-bottom-section').hide();
+    
     // slide toggle bottom sections.
     $('.dropdown-btn').on('click', function() {
         $el = $(this);
